614d367c00230d14f9b0e3c7da98545fef4c9cce
Davidmide Adenusi <Davidmide07@gmail.com>
2023-03-11 12:36:07 +0100

    complete

--- tracker.js
+++ tracker.js
@@ -1,27 +1,12 @@
 // grabing elements
-// let apiUrl = "https://geo.ipify.org/service/account-balance?apiKey=at_lwlL8xAPxaPb42jc8LaiIhkSpWOzh";
 
-// const byPassCorsUrl = "https://cors.anywhere.herokuapp.com/"
-
-// changes
-// API key
 const secretApi = "at_lwlL8xAPxaPb42jc8LaiIhkSpWOzh";
 
-const byPassCorsUri = "https://cors.anywhere.herokuapp.com/";
-const apiUri = "https://geo.ipify.org/service/account-balance"
-let currentVersion = "v1";
-
-const headersOption = {
-    headers : {
-        "Access-Control-Allow-Origin" : "*"
-    } 
-}
-// changes made
+const apiUri = "https://geo.ipify.org/api/v2/country";
 
 
-const headerOption = {
-    header: { "Access-control=Allow-origin": "*" }
-}
+
+
 
 const formEl = document.querySelector("#form");
 const inputIp = document.querySelector("#input-ip");
@@ -30,16 +15,7 @@ const locationEl = document.querySelector("#location-el");
 const timeZoneEl = document.querySelector("#timezone-el");
 const ispEl = document.querySelector("#isp-el");
 
-// testing grapped elements
-console.log(formEl)
-console.log(inputIp)
-console.log(ipEl)
-console.log(locationEl)
-console.log(timeZoneEl)
-console.log(ispEl)
-
-
-
+// intializing Map with the Leafletjs library
 
 let map = L.map("map", {
     "center": [0, 0],
@@ -57,35 +33,101 @@ let map = L.map("map", {
 // e represent an array containing two values which are lat and long
 function upDateMap(e) {
     e = [42, 42]
-    map.SetView(e, 13);
+    map.setView(e, 13);
     L.marker(e).addTo(map)
 
 }
 
-// function that get Ip detail and validate the input fill
+
+// function that get Ip detail and send it to the API for response
+
 function getIpDetails(defult_ip) {
 
-    if (defult_ip !== Number || defult_ip == null) {
-        let Url = `${byPassCorsUrl} ${apiUrl}`;
-        // change the link combination
-    } else {
-        // change link combination
-        let Url = `${byPassCorsUrl}${apiUrl}${defult_ip}`
+    if (defult_ip == undefined) {
+
+        // this return the ip address of the device the site is open with
+        let $url = `${apiUri}?apiKey=${secretApi}`;
+        fetchApi($url);
+
     }
 
-    fetch(apiUrl,headerOption)
-        .then(result => result.json)
-        .then(data => console.log(data))
+    else {
+        // get the input value and send to the API for response using the agument for the fechAPi function
+        let $url = `${apiUri}?apiKey=${secretApi}&ipAddress=${defult_ip}`;
+        fetchApi($url);
+    }
+}
+
+
+
+
+
+const fetchApi = (url) => {
+    fetch(url)
+        .then(result => result.json())
+        .then(data => {
+            console.log(data)
+            ipEl.innerHTML = `${data.ip}`
+            locationEl.innerHTML = `${data.location.region}, ${data.location.country}`
+            timeZoneEl.innerHTML = `${data.location.timezone}`
+            ispEl.innerHTML = `${data.isp}`
+            // upDateMap(${data})
+        })
+        .catch(err => alert("Oop! an error occurred"))
 
 }
 
-getIpDetails()
+// window.addEventListener("load", upDateMap);
+getIpDetails();
+
+// When the form is submitted get the input value and validate it before send it
+formEl.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+    console.log(inputIp.value)
+
+
+
+    if (inputIp.value !== "" || inputIp.value === Number) {
+        console.log(inputIp.value);
+        getIpDetails(inputIp.value);
+
+    }
+
+    else {
+
+        inputIp.classList.add("err-indicator");
+        // alert("TYou didn't put in an ip or the one you put in is wrong ")
+
+
+    }
+
+
+})
+
+
+// function getIpDetails(defult_ip) {
+
+//     if (defult_ip !== Number || defult_ip == null) {
+//         let Url = `${byPassCorsUrl} ${apiUrl}`;
+//         // change the link combination
+//     } else {
+//         // change link combination
+//         let Url = `${byPassCorsUrl}${apiUrl}${defult_ip}`
+//     }
+
+//     fetch(apiUrl,headerOption)
+//         .then(result => result.json)
+//         .then(data => console.log(data))
+
+// }
+
 
 // update function it will take in the map object and a parameter to be able to pass in the lat and log gotten from the Api to the map object when called
 // it must have defult value that will update the map on load
 
 // 2 function
-// A function that get 
+// A function that get
 
 
 
